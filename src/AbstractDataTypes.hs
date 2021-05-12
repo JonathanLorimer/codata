8be9c9381f2e05b92ec8357880c1e1ae563aa7b6
Jonathan Lorimer <jonathan_lorimer@mac.com>
2021-05-12 11:39:12 -0400

    additional ADT implementaitons

--- src/AbstractDataTypes.hs
+++ src/AbstractDataTypes.hs
@@ -6,6 +6,7 @@
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE ImplicitParams #-}
+{-# LANGUAGE QuantifiedConstraints #-}
 module AbstractDataTypes where
 
 import Prelude
@@ -59,27 +60,75 @@ test  =
   runSetImp @Int \(MkSet empty _ insert contains unionImp) ->
     contains (insert empty 1 `unionImp` insert empty 2) 1
 
--- Using a typeclass v-table
-type IsSet rep a = ?setProxy :: Proxy (ClassySet rep a)
+-- Explicit Classy Example
+data List a = Nil | a `Cons` List a
 
-class ClassySet rep a where
-  cEmpty :: IsSet rep a => rep a
-  cIsEmpty :: IsSet rep a => rep a -> Bool
-  cInsert :: IsSet rep a => rep a -> a -> rep a
-  cContains :: IsSet rep a => rep a -> a -> Bool
-  cUnion :: IsSet rep a => rep a -> rep a -> rep a
+class ListOps t where
+  nil :: t a
+  adjoin :: a -> t a -> t a
+  null :: t a -> Bool
+  headMay :: t a -> Maybe a
+  tailMay :: t a -> Maybe (t a)
+  equal :: Eq a => t a -> t a -> Bool
 
-instance Eq a => ClassySet [] a where
+instance ListOps List where
+  nil = Nil
+  adjoin = Cons
+  null = \case Nil -> True; _ -> False
+  headMay = \case Nil -> Nothing; x `Cons` _ -> Just x
+  tailMay = \case Nil -> Nothing; _ `Cons` xs -> Just xs
+  equal Nil Nil = True
+  equal (x`Cons`xs) (y`Cons`ys) = x == y && xs `equal` ys
+  equal _ _ = False
+
+testListOps :: (forall l. ListOps l, Num a, Eq a) => t a -> Bool
+testListOps l = equal (1 `adjoin` nil) (1 `adjoin` (2 `adjoin` (3 `adjoin` l)))
+
+class ClassySet rep where
+  cEmpty :: rep a
+  cIsEmpty :: rep a -> Bool
+  cInsert :: rep a -> a -> rep a
+  cContains :: Eq a => rep a -> a -> Bool
+  cUnion :: Eq a => rep a -> rep a -> rep a
+
+instance ClassySet [] where
   cEmpty = []
-  cIsEmpty = (== [])
+  cIsEmpty = \case [] -> True; _ -> False;
   cInsert = flip (:)
   cContains = flip elem
   cUnion = union
 
-runClassySet :: forall a b . Eq a => (forall rep. (IsSet rep a, ClassySet rep a) => b) -> b
-runClassySet f = let ?setProxy = Proxy @(ClassySet [] a) in f
+runClassySet :: (forall rep. ClassySet rep => rep a -> b) -> b
+runClassySet f = f []
 
-test' :: Bool
-test' =
-  runClassySet @Int $
-    cContains (cInsert cEmpty 1 `cUnion` cInsert cEmpty 2) 1
+testClassySet :: Bool
+testClassySet =
+  runClassySet @Int $ \s ->
+    cContains (cInsert s 1 `cUnion` cInsert s 2) 1
+
+-- Using only typeclass v-table
+type IsSet rep a = ?setProxy :: Proxy (VTableSet rep a)
+
+class VTableSet rep a where
+  vEmpty :: IsSet rep a => rep a
+  vIsEmpty :: IsSet rep a => rep a -> Bool
+  vInsert :: IsSet rep a => rep a -> a -> rep a
+  vContains :: IsSet rep a => rep a -> a -> Bool
+  vUnion :: IsSet rep a => rep a -> rep a -> rep a
+
+instance Eq a => VTableSet [] a where
+  vEmpty = []
+  vIsEmpty = (== [])
+  vInsert = flip (:)
+  vContains = flip elem
+  vUnion = union
+
+runVTableSet :: forall a b . Eq a => (forall rep. (IsSet rep a, VTableSet rep a) => b) -> b
+runVTableSet f = let ?setProxy = Proxy @(VTableSet [] a) in f
+
+testVTableSet :: Bool
+testVTableSet =
+  runVTableSet @Int $
+    vContains (vInsert vEmpty 1 `vUnion` vInsert vEmpty 2) 1
+
+
